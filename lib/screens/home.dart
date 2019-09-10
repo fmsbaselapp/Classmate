6e9cd771aba5ec3dca27a9d1c4a1c579641a7e1e
Timon <fmsbaselapp@gmail.com>
2019-09-10 23:47:52 +0200

    small ui anpasungen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,56 +14,70 @@ class HomeScreen extends StatelessWidget {
         builder: (context, snapshot) {
           //check if snapshot has data
           if (!snapshot.hasData) {
-            return Text("Loading..", style: Theme.of(context).textTheme.title);
+            return Text("Loading..",
+                style: Theme.of(context).textTheme.title); //todo loading
 
             //if snapshot has data:
           } else {
-            //listview builder
-            return ListView.builder(
-              //document counter
-              itemCount: snapshot.data.documents.length,
-              itemBuilder: (context, indexDocument) {
-                
-                //array counter
-               Map<String, dynamic> ausfallCounter =
-                   snapshot.data.documents[indexDocument].data;
-                return Column(
-                  
-                  children: <Widget>[
-                    ListTile(title: Text(snapshot.data.documents[indexDocument].documentID),),
-                    ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: ausfallCounter.length,
-                      itemBuilder: (context, index) {
-                        List<String> ausfall =
-                            List.from(snapshot.data.documents[indexDocument].data[index.toString()]);
+            if (snapshot.data.documents.length == 0) {
+              print('keine Dokumente');
+              return Center(
+                child: Text(
+                  'keine Ausfälle',
+                  style: Theme.of(context).textTheme.title,
+                ),
+              );
+            } else {
+              //listview builder
 
-                        return Card(
-                          child: Column(
-                            children: <Widget>[
-                              Text(
-                                ausfall[0], //first element from array
-                                style: Theme.of(context).textTheme.subtitle,
-                              ),
-                              Text(
-                                ausfall[1], //second element from array
-                                style: Theme.of(context).textTheme.subtitle,
-                              ),
-                              Text(
-                                ausfall[2], //third element from array
-                                style: Theme.of(context).textTheme.subtitle,
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  ],
-                );
-              },
-            );
+              return ListView.builder(
+                //document counter
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, indexDocument) {
+                  //array counter
+                  Map<String, dynamic> ausfallCounter =
+                      snapshot.data.documents[indexDocument].data;
+                  return Column(
+                    children: <Widget>[
+                      //titel Wochentag
+                      ListTile(
+                        title: Text(
+                            snapshot.data.documents[indexDocument].documentID, style: Theme.of(context).textTheme.button,),
+                      ),
+                      ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: ausfallCounter.length,
+                        itemBuilder: (context, index) {
+                          List<String> ausfall = List.from(snapshot.data
+                              .documents[indexDocument].data[index.toString()]);
+
+                          return Card(
+                            child: Column(
+                              children: <Widget>[
+                                Text(
+                                  ausfall[0], //first element from array
+                                  style: Theme.of(context).textTheme.subhead,
+                                ),
+                                Text(
+                                  ausfall[1], //second element from array
+                                  style: Theme.of(context).textTheme.subhead,
+                                ),
+                                Text(
+                                  ausfall[2], //third element from array
+                                  style: Theme.of(context).textTheme.subhead,
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            }
           }
         },
       ),
